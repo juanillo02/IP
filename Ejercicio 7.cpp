2e3d011a1b31163ada1c4cee5cccfaa1ef732784
Juan Acosta <116093967+juanillo02@users.noreply.github.com>
2023-10-26 20:52:56 +0200

    Add files via upload

---
+++ Ejercicio 7.cpp
@@ -0,0 +1,9 @@
+//Ejercicio 7
+#include<stdio.h>
+int main(){
+	printf("El tamano en byte del char es %i byte \n", sizeof(char));
+	printf("El tamano en bytes del entero es %i bytes\n", sizeof(int));
+	printf("El tamano del float es %i bytes\n", sizeof(float));
+	printf("El tamano del double es %i bytes\n", sizeof(double));
+	return 0;
+}
