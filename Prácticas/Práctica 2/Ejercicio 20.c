04ea97256eff117414cef155d22e80686ab14510
Juan Acosta <116093967+juanillo02@users.noreply.github.com>
2023-10-27 10:19:55 +0200

    Update and rename Ejercicio 20.cpp to Prácticas/Práctica 2/Ejercicio 20.c

--- Prácticas/Práctica 2/Ejercicio 20.c
+++ Prácticas/Práctica 2/Ejercicio 20.c
@@ -2,20 +2,24 @@
 
 #include<stdio.h>
 
-int main(){
+int main()
+{
 	int a, i, div;
 	i=1;
 	printf("Introduce un numero: ");
 	scanf("%i", &a);
-	while (i<=a){
+	while (i<=a)
+	{
 		div=a/i;
 		i++;
 	}
-	if (div!=a && div!=1){
+	if (div!=a && div!=1)
+	{
 		printf("El numero %i es primo.", a);
 	}
-	else {
+	else
+	{
 	("El numero %i no es primo.", a);
-}
-return 0;
+	}
+	return 0;
 }
