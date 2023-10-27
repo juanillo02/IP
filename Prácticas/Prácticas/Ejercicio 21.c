509bfd22bf9884d4ae9aa759b3bb76e28daf981f
Juan Acosta <116093967+juanillo02@users.noreply.github.com>
2023-10-27 10:44:08 +0200

    Update and rename Prácticas/Ejercicio 21.cpp to Prácticas/Prácticas/Ejercicio 21.c

--- Prácticas/Prácticas/Ejercicio 21.c
+++ Prácticas/Prácticas/Ejercicio 21.c
@@ -9,7 +9,8 @@ float mult (float, float);
 float div (float, float);
 char menu(float, float);
 
-int main(){
+int main()
+{
 	float a, b;
 	printf("Introduce dos numeros:\n");
 	scanf("%i%i", &a, &b);
@@ -17,33 +18,39 @@ int main(){
 	return 0;
 }
 
-float suma(float x, float y){
+float suma(float x, float y)
+{
 	float s;
 	s=x+y;
 	return s;
 }
 
-float resta(float x, float y){
+float resta(float x, float y)
+{
 	float r;
 	r=x+y;
 	return r;
 }
 
-float mult(float x, float y){
+float mult(float x, float y)
+{
 	float m;
 	m=x+y;
 	return m;
 }
 
-float div(float x, float y){
+float div(float x, float y)
+{
 	float d;
 	d=x+y;
 	return d;
 }
 
-char menu(float x, float y){
+char menu(float x, float y)
+{
 	char c;
-	switch (c){
+	switch (c)
+	{
 		case '+': printf ("La suma %i+%i=%i", x, y); break;
 		case '-': printf ("La resta %i-%i=%i", x, y); break;
 		case '*': printf ("La multiplicacion %i*%i=%i", x, y); break;
