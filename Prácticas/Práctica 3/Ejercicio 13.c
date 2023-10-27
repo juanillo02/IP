b3e9b17e8617ce7ef369bb8fbdfd4f15eef37d7d
Juan Acosta <116093967+juanillo02@users.noreply.github.com>
2023-10-27 10:48:13 +0200

    Update and rename Prácticas/Ejercicio 13.cpp to Prácticas/Práctica 3/Ejercicio 13.c

--- Prácticas/Práctica 3/Ejercicio 13.c
+++ Prácticas/Práctica 3/Ejercicio 13.c
@@ -4,27 +4,31 @@
 
 void add_divRef(int, int*);
 
-int main(){
+int main()
+{
 	int s, i;
-	for(i=1;i<1000;i++){
-    add_divRef(i, &s);
-    if(i==s){
-    	printf("%i\n", i);
+	for(i=1;i<1000;i++)
+	{
+    		add_divRef(i, &s);
+    		if(i==s)
+		{
+    			printf("%i\n", i);
+		}
 	}
-}
 	return 0;
 }
 
 //Cabecera void add_divRef(int num, int* suma)
 //Precondicion a=1000
 //Postcondicion devuelve la suma de los divisores
-
-
-void add_divRef(int num, int *suma){
+void add_divRef(int num, int *suma)
+{
 	int j;
 	*suma=0;
-	for(j=1;j<num;j++){
-		if(num%j==0){
+	for(j=1;j<num;j++)
+	{
+		if(num%j==0)
+		{
 			*suma+=j;
 		}
 	}
