0c705209d41e11d88a4887392db04c7fc19f8728
Juan Acosta <116093967+juanillo02@users.noreply.github.com>
2023-10-27 10:30:52 +0200

    Update and rename Ejercicio 5.cpp to Prácticas/Práctica 3/Ejercicio 05.c

--- Prácticas/Práctica 3/Ejercicio 05.c
+++ Prácticas/Práctica 3/Ejercicio 05.c
@@ -1,16 +1,20 @@
 //Ejercicio 5
 
 #include<stdio.h>
-int main(){
+int main()
+{
 	int a, b;
 	char x;
-	printf("Introduce un simbolo: "); //El simbolo tiene que ir antes de las variables. Se podria ponerr despues pero usando fflush (stdin) para eliminar la entrada del teclado.
-	scanf("%c",&x); //Otra opcion es escribir scanf("%c",&x); con un espacio delante de %c. Se puede utilizar scanf("%c",&x); o tambien oper = getchar ();
+	printf("Introduce un simbolo: ");
+	//El simbolo tiene que ir antes de las variables. Se podria poner despues pero usando fflush (stdin) para eliminar la entrada del teclado.
+	scanf("%c",&x);
+	//Otra opcion es escribir scanf("%c",&x); con un espacio delante de %c. Se puede utilizar scanf("%c",&x); o tambien oper = getchar ();
 	printf("Introduce un valor para a: ");
 	scanf("%i", &a);
 	printf("Introduce un valor para b: ");
 	scanf("%i", &b);
-	switch (x){
+	switch (x)
+	{
 		case '+': printf("La suma es: %i\n", a+b); break;
 		case '-': printf("Las restas son: %i, %i\n", a-b, b-a); break;
 		case '*': printf("La multiplicacion es: %i\n", a*b); break;
