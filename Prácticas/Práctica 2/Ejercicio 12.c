43cac80d75c5a84734df20a6b9abb9ef7be1671f
Juan Acosta <116093967+juanillo02@users.noreply.github.com>
2023-10-27 09:59:25 +0200

    Update and rename Ejercicio 12.cpp to Prácticas/Práctica 2/Ejercicio 12.c

---
+++ Prácticas/Práctica 2/Ejercicio 12.c
@@ -0,0 +1,31 @@
+// Ejercicio 12
+
+#include<stdio.h>
+
+int main()
+{
+	int a, b, s, aux;
+	printf ("Introduce dos numeros: ");
+	scanf("%i%i", &a,&b);
+	if (a==b)
+	{
+		printf ("Los numeros %i y %i son iguales.", a, b);
+	}
+	else
+	{
+		if (a<b)
+		{
+			aux=a;
+			a=b;
+			b=aux;
+		}
+		s=b+1;
+		printf ("Los numeros comprendidos entre %i y %i son: ", a, b);
+		while (s<a)
+		{
+			printf("%i, ", s);
+			s++;
+		}
+	}
+	return 0;
+}
