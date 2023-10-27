84dc920648b43d67c0468dbe9879e3eef21df8f5
Juan Acosta <116093967+juanillo02@users.noreply.github.com>
2023-10-27 10:42:23 +0200

    Update and rename Prácticas/Ejercicio 4.cpp to Prácticas/Práctica 3/Ejercicio 04.c

--- Prácticas/Práctica 3/Ejercicio 04.c
+++ Prácticas/Práctica 3/Ejercicio 04.c
@@ -2,13 +2,14 @@
 
 #include<stdio.h>
 
-int main(){
+int main()
+{
 	int *x, y;
 	printf("Introduzca un valor:\n");
 	scanf("%i", &y);
 	x=&y;
 	printf ("La direccion del entero es %p y el contenido %d\n", &y, x);
-    printf ("La direccion del puntero es %p y el contenido %p\n", &x, x);
-    printf ("El valor referenciado por el puntero es %d\n", *x);
+    	printf ("La direccion del puntero es %p y el contenido %p\n", &x, x);
+    	printf ("El valor referenciado por el puntero es %d\n", *x);
 	return 0;
 }
