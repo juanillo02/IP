d1b0d749cfd4933d4810f861a90241b5664a3d03
Juan Acosta <116093967+juanillo02@users.noreply.github.com>
2023-10-27 10:16:35 +0200

    Update and rename Ejercicio 18.cpp to Prácticas/Práctica 2/Ejercicio 18.c

--- Prácticas/Práctica 2/Ejercicio 18.c
+++ Prácticas/Práctica 2/Ejercicio 18.c
@@ -2,7 +2,8 @@
 
 #include<stdio.h>
 
-int main(){
+int main()
+{
 	int letras, lineas, palabras;
 	char c;
 	printf("Introduce algo: ");
@@ -10,17 +11,22 @@ int main(){
 	letras=1;
 	lineas=1;
 	palabras=1;
-	while ((c=getchar())!= '*'){
-		if(c== ' '){
-		palabras ++;}
-		else {
-			if(c== '\n'){
+	while ((c=getchar())!= '*')
+	{
+		if(c== ' ')
+		{
+		palabras ++;
+		}
+		else
+		{
+			if(c== '\n')
+			{
 				lineas++;
 				palabras++;
 			}
-			letras++;
-			}
+		letras++;
 		}
+	}
 	printf ("El numero de lineas es: %d. \n", lineas);
 	printf ("El numero de letras es: %d. \n", letras);
 	printf ("El numero de palabras es: %d. \n",  palabras);
