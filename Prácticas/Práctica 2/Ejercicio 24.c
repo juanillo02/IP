7868255f1978df04c2ea8ea0b4dbe0329947c459
Juan Acosta <116093967+juanillo02@users.noreply.github.com>
2023-10-27 10:23:45 +0200

    Update and rename Ejercicio 24.cpp to Prácticas/Práctica 2/Ejercicio 24.c

--- Prácticas/Práctica 2/Ejercicio 24.c
+++ Prácticas/Práctica 2/Ejercicio 24.c
@@ -2,24 +2,30 @@
 
 #include<stdio.h>
 
-int main(){
+int main()
+{
 	int a, b, i, div;
 	printf ("Introduce un rango:\n");
 	scanf("%i%i", &a, &b);
 	i=1;
-	if (a>b){
+	if (a>b)
+	{
 		printf("No se pueden dar los numeros ya que el rango esta mal expresado.");
 	}
-	else{
-		while (a<=b){
-			do{
-			div=a&i;
-			i++;
-		} while (i<=a);
-			if (div!=a && div!=1){
-		    printf("%i ", a);
-		    a++;
-	    }
+	else
+	{
+		while (a<=b)
+		{
+			do
+			{
+				div=a&i;
+				i++;
+			} while (i<=a);
+			if (div!=a && div!=1)
+			{
+		    		printf("%i ", a);
+		    		a++;
+	    		}
 		}
 	}
 	return 0;
