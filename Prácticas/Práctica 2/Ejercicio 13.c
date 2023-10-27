cf56ce5d4af4a8cad78dd8c5c23fa50bed0959eb
Juan Acosta <116093967+juanillo02@users.noreply.github.com>
2023-10-27 10:00:10 +0200

    Update and rename Ejercicio 13.cpp to Prácticas/Práctica 2/Ejercicio 13.c

--- Prácticas/Práctica 2/Ejercicio 13.c
+++ Prácticas/Práctica 2/Ejercicio 13.c
@@ -2,18 +2,20 @@
 
 #include<stdio.h>
 
-int main(){
+int main()
+{
 	int a, suma;
 	float i, media;
 	i=0;
 	suma=0;
-	do {
+	do
+	{
 		printf("Introduce un numero: ");
-     	scanf("%i", &a);
-     	i++;
-     	suma=suma+a;
-}	while (a>0);
-media=suma/(i=i-1);
+     		scanf("%i", &a);
+     		i++;
+     		suma=suma+a;
+	}while (a>0);
+	media=suma/(i=i-1);
 	printf("La media es: %.1f", media);
 	return 0;
 }
