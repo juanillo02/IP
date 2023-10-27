feffcc24b2902160c9ff07e2277d6dcb871bd149
Juan Acosta <116093967+juanillo02@users.noreply.github.com>
2023-10-27 10:14:27 +0200

    Update and rename Ejercicio 16.cpp to Prácticas/Práctica 2/Ejercicio 16.c

--- Prácticas/Práctica 2/Ejercicio 16.c
+++ Prácticas/Práctica 2/Ejercicio 16.c
@@ -2,10 +2,13 @@
 
 #include<stdio.h>
 
-int main(){
+int main()
+{
 	int a, b;
-	for (a=1;a<=10;a++){
-		for (b=1;b<=10;b++){
+	for (a=1;a<=10;a++)
+	{
+		for (b=1;b<=10;b++)
+		{
 			printf("%i*%i=%i\n", a, b, a*b);
 		}
 	}
