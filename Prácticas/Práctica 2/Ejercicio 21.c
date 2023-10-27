83d3f62769c8e655a1e9fb6ab86e000b64fb3fa6
Juan Acosta <116093967+juanillo02@users.noreply.github.com>
2023-10-27 10:21:03 +0200

    Update and rename Ejercicio 21.cpp to Prácticas/Práctica 2/Ejercicio 21.c

---
+++ Prácticas/Práctica 2/Ejercicio 21.c
@@ -0,0 +1,32 @@
+// Ejercicio 21
+
+#include<stdio.h>
+
+int main()
+{
+	int n, a, b, c, i;
+	printf("Introduce un valor para la serie de Fibonnaci: ");
+	scanf("%i", &n);
+	if (n<0)
+	{ 
+	printf("ERROR");
+	}
+	else
+	{
+		if(n==1 || n==2)
+		{
+			printf("La solucion de la serie es 1.");
+		}
+		else
+		{
+			for(i==3;i<=n;i++)
+			{
+				c=a+b;
+				a=b;
+				b=c;
+				printf("%i, ", c);
+			}
+		}
+	}
+	return 0;
+}
