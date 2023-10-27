168fd6a39d0e1839d446a49e9907ba16e7d490a5
Juan Acosta <116093967+juanillo02@users.noreply.github.com>
2023-10-27 10:43:13 +0200

    Update and rename Prácticas/Ejercicio 3.cpp to Prácticas/Práctica 3/Ejercicio 03.c

--- Prácticas/Práctica 3/Ejercicio 03.c
+++ Prácticas/Práctica 3/Ejercicio 03.c
@@ -8,52 +8,54 @@ int resta (int, int);
 int producto (int, int);
 int division (int, int);
 
-int main(){
+int main()
+{
 	int opcion, s, r, p, d, a, b;
 	printf("Intoduce dos numeros enteros: \n");
 	scanf("%i%i", &a, &b);
 	printf ("Elige que deseas hacer:\n1.- Suma.\n2.- Resta.\n3.-Producto.\n4.- Division.\n");
 	scanf("%i", &opcion);
-	switch (opcion){
-		case 1: 
-		s=suma(a,b);
+	switch (opcion)
+	{
+		case 1: s=suma(a,b);
 		printf("La suma es: %i", s);
 		break;
-		case 2: 
-		r=resta(a,b);
+		case 2: r=resta(a,b);
 		printf("La resta es: %i", r);
 		break;
-		case 3: 
-		p=producto(a,b);
+		case 3: p=producto(a,b);
 		printf("El producto es: %i", p);
 		break;
-		case 4:
-		d=division(a,b);
+		case 4:d=division(a,b);
 		printf("La division es: %i", d);
 		break;
 	}
 	return 0;
 }
 
-int suma (int a, int b){
+int suma (int a, int b)
+{
 	int suma;
 	suma=a+b;
 	return suma;
 }
 
-int resta (int a, int b){
+int resta (int a, int b)
+{
 	int resta;
 	resta=a-b;
 	return resta;
 }
 
-int producto (int a, int b){
+int producto (int a, int b)
+{
 	int producto;
 	producto=a*b;
 	return producto;
 }
 
-int division (int a, int b){
+int division (int a, int b)
+{
 	int division;
 	division=a/b;
 	return division;
