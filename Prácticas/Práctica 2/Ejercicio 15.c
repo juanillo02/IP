c2d4d2f20f01807ebb8a50ad7d353c6d86539323
Juan Acosta <116093967+juanillo02@users.noreply.github.com>
2023-10-27 10:13:52 +0200

    Update and rename Ejercicio 15.cpp to Prácticas/Práctica 2/Ejercicio 15.c

--- Prácticas/Práctica 2/Ejercicio 15.c
+++ Prácticas/Práctica 2/Ejercicio 15.c
@@ -2,18 +2,22 @@
 
 #include<stdio.h>
 #include<math.h>
- int main(){
+ int main()
+{
  	int b, e, cont=0, pot=1;
  	puts("Introduce una base y un exponente: ");
  	scanf("%d%d", &b, &e);
- 	while (cont < abs(e)){
+ 	while (cont < abs(e))
+	{
  		pot*=b;
  		cont++;
 	 }
-	 if(e<0) {
+	 if(e<0)
+	 {
 	 	printf("La potencia es %.3f.", (float)1/pot);
 	 }
-	 else{
+	 else
+	 {
 	 	printf("La potencia es %d", pot);
 	 }
 	 if (e==0){
