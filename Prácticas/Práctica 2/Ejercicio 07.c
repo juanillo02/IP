58da599ccb71d0008b8e54f3175c9060c48a2299
Juan Acosta <116093967+juanillo02@users.noreply.github.com>
2023-10-27 10:33:05 +0200

    Update and rename Ejercicio 7.cpp to Prácticas/Práctica 2/Ejercicio 07.c

--- Prácticas/Práctica 2/Ejercicio 07.c
+++ Prácticas/Práctica 2/Ejercicio 07.c
@@ -2,7 +2,8 @@
 
 #include<stdio.h>
 
-int main (){
+int main ()
+{
 	int d, m, a;
 	printf("Introduce un dia: ");
 	scanf("%i", &d);
@@ -10,7 +11,8 @@ int main (){
 	scanf("%i", &m);
 	printf("Introduce un ano: ");
 	scanf("%i", &a);
-	switch (m){
+	switch (m)
+	{
 		case 1:
 		case 3:
 		case 5:
@@ -18,33 +20,38 @@ int main (){
 		case 8:
 		case 10:
 		case 12:
-			if (d<0 || d>31){
+			if (d<0 || d>31)
+			{
 				printf ("Fecha incorrecta.");
 			}
-			else{
+			else
+			{
 				printf ("Fecha correcta.");
 			}break;
 		case 4:
 		case 6:
 		case 9:
 		case 11:
-			if (d<0 || d>30){
+			if (d<0 || d>30)
+			{
 				printf("Fecha incorrecta.");
 			}
-			else{
+			else
+			{
 				printf("Fecha correcta.");
-		    } break;
+		    	}break;
 		case 2: 
-		if (a%4==0 && a%100!=0 || a%400==0){
-			if (d>0 || d<30){
-				printf("La fecha es correcta.");
-	        }
-			else{
+			if (a%4==0 && a%100!=0 || a%400==0)
+			{
+				if (d>0 || d<30)
+				{
+					printf("La fecha es correcta.");
+	        		}
+			else
+			{
 				printf("La fecha es incorrecta.");
-		    }
-    } break;
+		    	}}break;
 		default: printf("El mes introducido es incorrecto.");
 	}
-	
 	return 0;
 }
