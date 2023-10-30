b113334f1bed03c82a4c7a0746688ba9558140f2
Juan Acosta <116093967+juanillo02@users.noreply.github.com>
2023-10-30 08:44:08 +0100

    Update and rename Ejemplo break y continue, no utilizar en estos casos.cpp to Ejemplo break y continue, no utilizar en estos casos.c

--- Prácticas/Ejemplo break y continue, no utilizar en estos casos.c
+++ Prácticas/Ejemplo break y continue, no utilizar en estos casos.c
@@ -2,10 +2,15 @@
 
 #include<stdio.h>
 
-int main(){
+int main()
+{
 	int i;
-	for(i=1; i<10; i++){
-		if (i==4) {puts("aqui hay un salto");continue;}
+	for(i=1; i<10; i++)
+	{
+		if (i==4)
+		{
+			puts("aqui hay un salto");continue;
+		}
 		if (i==7) break;
 		printf("El valor de i es %d\n", i);
 	}
