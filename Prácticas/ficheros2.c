7357895b876c3e31817aec0f2b2f872f6e35c978
Juan Acosta <116093967+juanillo02@users.noreply.github.com>
2023-10-30 09:04:50 +0100

    Update and rename ficheros2.cpp to ficheros2.c

---
+++ Prácticas/ficheros2.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+	FILE*f;
+	char car;
+	if((f=fopen("fichcar","r"))==NULL)
+	{
+		printf("Error apertura");
+	//	exit (1);
+	}
+	else
+	{
+		while (!EOF==(f))
+		{
+			car=getchar();
+			putc(car,f);
+		}
+		fclose(f);
+	}
+	return 0;
+}
