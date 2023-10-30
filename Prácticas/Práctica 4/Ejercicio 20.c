ec498a5bb29e98651671d4ea8fb1da0d2ee87b44
Juan Acosta <116093967+juanillo02@users.noreply.github.com>
2023-10-30 08:55:09 +0100

    Update and rename Prácticas/Ejercicio 20.cpp to Prácticas/Práctica 4/Ejercicio 20.c

--- Prácticas/Práctica 4/Ejercicio 20.c
+++ Prácticas/Práctica 4/Ejercicio 20.c
@@ -4,34 +4,27 @@
 #include<string.h>
 #include<stdlib.h>
 
-int cuenta (char *, char);
+int cuenta (char *);
 
-int main(){
-	/*char car, resp;
-	char cad[10];
-	puts("Introduce caracter a buscar en cad");
-	car=getchar();
-	//scanf(%c",&car);
-	printf()
-	do{
-		fflush
-	} while
-	*/
-	
-	int  a, b;
+int main()
+{
+	int  a;
 	char v[30];
 	puts("Escribe una cadena:");
 	fgets(v,30,stdin);
-	b=cuenta(v,a);
+	a=cuenta(v);
 	printf("El caracter %i se repite %i veces.", a, b);
 	return 0;
 }
 
-int cuenta (char *cad, char c){
+int cuenta (char *cad)
+{
 	int i, j=0;
+	char c;
 	printf("Introduce el caracter que desea contar: ");
-	scanf("%i", &c);
-	for(i=0;cad[i]!='\0';i++){
+	scanf("%c", &c);
+	for(i=0; cad[i] != '\0';i++)
+	{
 		if(cad[i]==c) j++;
 	}
 	return j;
