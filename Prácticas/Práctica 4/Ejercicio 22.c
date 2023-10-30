7ea40ce454fdf5128835a4d77ad4b5b98fef825f
Juan Acosta <116093967+juanillo02@users.noreply.github.com>
2023-10-30 08:56:11 +0100

    Update and rename Prácticas/Ejercicio 22.cpp to Prácticas/Práctica 4/Ejercicio 22.c

--- Prácticas/Práctica 4/Ejercicio 22.c
+++ Prácticas/Práctica 4/Ejercicio 22.c
@@ -6,22 +6,25 @@
 
 void invertir(char *);
 
-int main(){
+int main()
+{
 	int n; //numero de elementos
 	char *cadena; //puntero a char
-	do{
+	do
+	{
 		puts("Cuantos elementos quieres en la cadena?");
 		scanf("%i", &n);
 	} while(n<1);
 	//crear vector de manera dinamica
 	if ((cadena=(char *)malloc(n*sizeof(char)))==NULL)
 	puts("No se ha realizado reserva de memoria");
-	else{
+	else
+	{
 		printf("Introduce cadena de menos de %i caracteres\n", n);
 		fflush(stdin);
 		fgets(cadena,n,stdin);
 		if(cadena[strlen(cadena)-1]=='\n')cadena[strlen(cadena)-1]='\0';
-//		printf("La cadena original es: %c\n", *cadena);
+		//printf("La cadena original es: %c\n", *cadena);
 		invertir(cadena);
 		puts("La cadena es");
 		puts(cadena);
@@ -29,11 +32,13 @@ int main(){
 	return 0;
 }
 
-void invertir(char *cad){
+void invertir(char *cad)
+{
 	int i=0, j;
 	char aux;
 	j=strlen(cad)-1;
-	while(i<j){
+	while(i<j)
+	{
 		aux=cad[i];
 		cad[i]=cad[j];
 		cad[j]=aux;
