4ce673017f46d182550cf7762a26b6bdbaf79832
Juan Acosta <116093967+juanillo02@users.noreply.github.com>
2023-10-30 08:51:41 +0100

    Update and rename Prácticas/Ejercicio 17.cpp to Prácticas/Práctica 4/Ejercicio 17.c

--- Prácticas/Práctica 4/Ejercicio 17.c
+++ Prácticas/Práctica 4/Ejercicio 17.c
@@ -6,10 +6,11 @@
 void quitar_saltolinea(char *);
 int longitud(char []); // Se puede expresar de las dos maneras ya que se esta pasando direccion de memmoria
 
-int main(){
+int main()
+{
 	char nombre[15];
 	int i;
-	puts("¿Nombre? Menor que 15 caracteres ");  //puts=printf+'\n' para las cadenas. Si es una cadena de caracteres se puede utilizar printf y puts
+	puts("Â¿Nombre? Menor que 15 caracteres ");  //puts=printf+'\n' para las cadenas. Si es una cadena de caracteres se puede utilizar printf y puts
 	fgets(nombre,15,stdin);
 	quitar_saltolinea(nombre);
 	printf("El numero de caracteres es %d\n",longitud(nombre));
@@ -18,13 +19,15 @@ int main(){
 
 //gets(nombre);  scanf("%s",nombre); Son dos maneras de escribirlo
 
-void quitar_saltolinea(char *cad){
+void quitar_saltolinea(char *cad)
+{
 	int i;
 	for(i=0; cad[i]!='\0' && cad[i]!='\n';i++);
 	if(cad[i]=='\n') cad[i]='\0';
 }
 
-int longitud(char *cad){
+int longitud(char *cad)
+{
 	int i;
 	for(i=0;cad[i]!='\0';i++);
 	return i;
