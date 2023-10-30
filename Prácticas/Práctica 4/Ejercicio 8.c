926ba516e970c3e9bb288c3539e37e7cd6c31a9f
Juan Acosta <116093967+juanillo02@users.noreply.github.com>
2023-10-30 09:01:05 +0100

    Update and rename Prácticas/Ejercicio 8.cpp to Prácticas/Práctica 4/Ejercicio 8.c

--- Prácticas/Práctica 4/Ejercicio 8.c
+++ Prácticas/Práctica 4/Ejercicio 8.c
@@ -7,9 +7,11 @@
 
 void mostrar(int[], int);
 
-int main(){
+int main()
+{
 	int v[N], i;
-	for(i=0;i<N;i++){
+	for(i=0;i<N;i++)
+	{
 		printf("Introduce un valor para la posicion %i del vector:\n", i+1);
 		scanf("%i", &v[i]);
 	}
@@ -17,10 +19,13 @@ int main(){
 	return 0;
 }
 
-void mostrar(int vec[], int elem){
+void mostrar(int vec[], int elem)
+{
 	int j;
-	for(j=0;j<elem;j++){
-		if(vec[j]!=vec[elem]){
+	for(j=0;j<elem;j++)
+	{
+		if(vec[j]!=vec[elem])
+		{
 			printf("%i", vec[elem]);
 			elem++;
 		}
